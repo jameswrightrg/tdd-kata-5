cd8a7a4f7385f0e5825eaca94209a600247a5798
James Wright <james.wright@red-gate.com>
2017-06-09 14:19:01 +0100

    sorts 2 cards

--- tddkata5/CardSorter.cs
+++ tddkata5/CardSorter.cs
@@ -1,10 +1,24 @@
-﻿namespace tddkata5
+﻿using System;
+using System.Linq;
+
+namespace tddkata5
 {
     public class CardSorter
     {
         public static string Sort(string input)
         {
-            return input;
+            return GetReversedString(input);
+        }
+
+        private static string GetReversedString(string input)
+        {
+            var result = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result += input[i];
+            }
+
+            return result;
         }
     }
 }

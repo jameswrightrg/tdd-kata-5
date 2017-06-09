dbf9f670db0443520d9ddfc4b4be0942a59a48a9
James Wright <james.wright@red-gate.com>
2017-06-09 14:09:51 +0100

    [Red] swaps cards wrong way round

--- tddkata5.Tests/Class1.cs
+++ tddkata5.Tests/Class1.cs
@@ -18,5 +18,15 @@ namespace tddkata5.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void SwapsTwoCards()
+        {
+            var input = "32";
+            var expected = "23";
+            var output = CardSorter.Sort(input);
+
+            Assert.That(expected, Is.EqualTo(output));
+        }
     }
 }

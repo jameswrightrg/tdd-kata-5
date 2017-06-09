615aee519de8f010e14343f707242aaf8a478926
James Wright <james.wright@red-gate.com>
2017-06-09 14:06:53 +0100

    [Red] sorting single card

--- tddkata5.Tests/Class1.cs
+++ tddkata5.Tests/Class1.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace tddkata5.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void OneCardIsSorted()
+        {
+            var actual = "A";
+            var expected = CardSorter.Sort();
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
